eae9f40c465f91b7bd0cc5d2d3bd086ca6af03d2
Jie Liang <liangjie_wm@163.com>
2024-07-28 18:30:59 +0800

    set up sever, lack api

---
+++ server/db.config.js
@@ -0,0 +1,7 @@
+const dbName = "techtutor"
+const dbHost = "localhost"
+const dbPort = "27017"
+
+module.exports = {
+  url: `mongodb://${dbHost}:${dbPort}/${dbName}`,
+}
---
+++ server/models/productModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const CategorySchema = new Schema({ name: { type: String, required: true } })
+
+const ProductSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+})
+
+module.exports = mongoose.model("Product", ProductSchema)
